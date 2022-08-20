4a1f205d8937a73bbc16c0eab59ce654e6dec728
Maya Gans <jaffe.maya@gmail.com>
2022-08-19 18:00:13 -0600

    add chai

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,7 @@
 'use strict'
 
-var assert = require('assert');
+
+var assert = require('chai').assert;
 import { removeDuplicates } from "../src/removeDuplicates.js"
 import { pluck } from "../src/pluck.js"
 
